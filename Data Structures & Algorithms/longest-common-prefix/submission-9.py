e48e07ad921b3dbf89243cb7b59ed2afd17587ba
Sahil Kumar Rathour <23n0266@iitb.ac.in>
2026-04-03 01:16:05 +0530

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        
+        counter = 0 
+        letter_number = 0
+        text = ""
+        return_val = ""
+        
+        while letter_number <= len(strs[counter])-1:
+
+                if len(text)>0:
+                    if (strs[counter][letter_number]) != text:
+                            break
+                else:
+                    text = strs[counter][letter_number]
+
+                counter = counter + 1
+
+                if counter == len(strs):
+                    return_val = return_val + text
+                    text = ""
+                    counter = 0 
+                    letter_number = letter_number + 1
+
+        return return_val
+        
+      
+
+
+                
