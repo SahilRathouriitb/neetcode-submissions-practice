a2aa3e44ced63c55cb52214e79f06838245b3aff
Sahil Kumar Rathour <23n0266@iitb.ac.in>
2026-04-26 10:12:12 +0530

    Add: decode-string - submission-8

---
+++ Data Structures & Algorithms/decode-string/submission-8.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def decodeString(self,s: str) -> str:
+        stack = []
+        i = 0
+        
+
+        while i<len(s):
+
+            if s[i] != ']':
+                stack.append(s[i])
+
+            elif s[i] == ']':
+                store = []
+                # Now we will capture what is inside the bracket 
+                while stack[-1] != '[':
+                    k = stack.pop()
+                    store.append(k)
+                # This pop will remove the opening bracked
+                stack.pop()
+
+                # We need to count the number now 
+                num = 0
+                j = 1
+                while stack[-1] in [str(i) for i in range(0,10)]:
+                    digit = stack.pop()
+                    num = num + int(digit)*j
+                    j = j*10
+                    if len(stack) == 0:
+                        break
+                store.reverse()
+                store = store * num
+
+                for j in store:
+                    stack.append(j)
+            i = i + 1
+        
+        return "".join(stack)
+
+
+            
