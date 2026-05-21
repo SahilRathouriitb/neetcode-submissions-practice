2bee780802c27df914d562727d67f87ef588765f
Sahil Kumar Rathour <23n0266@iitb.ac.in>
2026-05-21 16:19:16 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-4

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-4.py
@@ -0,0 +1,40 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        min_cap = max(weights)
+        max_cap = 0
+        for i in weights:
+            max_cap += i
+
+        # we need to find out the suitable value in between min_cap and max_cap using binary search
+        lower = min_cap
+        upper = max_cap
+
+        while True:
+            mid = min_cap + (max_cap - min_cap)//2
+            if (min_cap == max_cap) or (mid == lower) or (mid == upper):
+                return mid
+            
+            i = 0
+            sum = 0
+            ships = 0
+
+            while i < len(weights):
+
+                sum = sum + weights[i]
+
+                if sum <= mid:
+                    i += 1
+                    if i == len(weights):
+                        ships += 1
+
+                elif sum > mid:
+                    sum = 0 
+                    ships += 1
+                
+
+            if (ships <= days) :
+                max_cap = mid 
+
+            elif (ships > days):
+                min_cap = mid + 1
+            
