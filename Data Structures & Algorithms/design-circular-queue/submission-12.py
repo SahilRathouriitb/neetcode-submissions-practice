0ef2b2937643bd095b5b461eae6882b3fb77eda8
Sahil Kumar Rathour <23n0266@iitb.ac.in>
2026-05-25 23:38:03 +0530

    Add: design-circular-queue - submission-12

---
+++ Data Structures & Algorithms/design-circular-queue/submission-12.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.start = Node(-1)
+        a = self.start
+        for i in range(k-1):
+            a.next = Node(-1)
+            a = a.next
+        
+        a.next = self.start
+
+        # Now after this much we have the circular queue
+
+        self.end = self.start
+        self.filled = 0 
+        self.max_allowed = k
+            
+
+    def enQueue(self, value: int) -> bool:
+        if self.filled < self.max_allowed:
+            if self.end.value != (-1):
+                self.end = self.end.next
+            self.end.value = value
+            self.filled += 1
+            return True
+
+        else:
+            return False
+        
+
+    def deQueue(self) -> bool:
+        if self.filled > 0:
+            self.start.value == -1
+            self.start = self.start.next 
+            self.filled -= 1
+            return True
+        else:
+            return False
+        
+
+    def Front(self) -> int:
+        if self.filled > 0:
+            return self.start.value
+        else:
+            return -1
+        
+
+    def Rear(self) -> int:
+        if self.filled > 0:
+            return self.end.value
+        else:
+            return -1
+        
+
+    def isEmpty(self) -> bool:
+        if self.filled == 0:
+            return True
+        else:
+            return False
+        
+
+    def isFull(self) -> bool:
+        if self.filled == self.max_allowed:
+            return True
+        else:
+            return False
