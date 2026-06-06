4bf1d14e35a9dddc22354c967cdde7a12d3dc12a
Sahil Kumar Rathour <23n0266@iitb.ac.in>
2026-06-06 23:57:01 +0530

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.py
@@ -0,0 +1,63 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        check = {}
+        store = None
+        
+        for i in t:
+                if i in check:
+                    check[i] = check[i] + 1
+                else:
+                    check[i] = 1
+        temp_check = check.copy()            
+
+        i = 0
+        while i < len(s):
+            
+            
+
+            if s[i] in temp_check:
+                left = i
+                
+                temp_check[s[i]] = temp_check[s[i]] - 1
+                if temp_check[s[i]] == 0:
+                    temp_check.pop(s[i])
+                
+                if len(temp_check) == 0:
+                    right = left
+                    if store == None:
+                            store = [left,right, right-left]
+                    elif (right-left < store[2]):
+                            store = [left,right, right-left]
+        
+
+                right = i + 1
+                while (right < len(s)) and (len(temp_check)>0):
+    
+                    if s[right] in temp_check:
+                        temp_check[s[right]] = temp_check[s[right]] - 1
+                        if temp_check[s[right]] == 0:
+                            temp_check.pop(s[right])
+
+                        if len(temp_check) == 0:
+                            if store == None:
+                                store = [left,right, right-left]
+                            elif (right-left < store[2]):
+                                store = [left,right, right-left]
+                            break
+
+                    right += 1
+                
+                temp_check = check.copy()
+
+            
+            i += 1
+        
+        if store == None:
+            return ""
+        else:
+
+            return s[store[0]:store[1]+1]
+                
+        
+        
